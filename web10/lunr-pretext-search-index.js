d15b29cefcac0acb22bf06215f2df7aba73de7b1
Jennifer Whitfield <jwhitfld@tamu.edu>
2026-03-31 14:56:31 +0000

    Latest build deployed.

---
+++ web10/lunr-pretext-search-index.js
@@ -0,0 +1,221 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "session1wks",
+  "level": "1",
+  "url": "#session1wks",
+  "type": "Article",
+  "number": "",
+  "title": "MATH 152 Hands-On, Grades Up",
+  "body": " MATH 152 Hands-On, Grades Up   For workspace between exercises, print this worksheet or download to your device by clicking the printer icon.     Session #10 (Sections ?? coming soon)  Directions: Answer each of the following as best you can. Feel free to discuss your thoughts about the questions with some peers around you. For additional practice problems and explanations, refer to the Math 152 textbook which can be found in your WebAssign Course.    Getting Warmed Up and Focused   For #1-8 below, read the statement and then circle \"true\" if the statement is correct and \"false\" if the statement is not correct.     True or False : The path represented by the parametric equations , where , is a circle of radius 1, with initial point at , terminal point at , and the path follows a counterclockwise direction.       True or False : The path represented by the parametric equations , where , is a circle of radius 1, with initial point at , terminal point at , and the path follows a clockwise direction.       True or False : The paths represented by and are the same paths.       True or False : The path represented by , where , starts at and moves in the clockwise direction twice around a circle centered at with a radius of 1.       True or False : Given and , the slope of the tangent for any value of is .       True or False : Given and , the slope of the tangent line at is .       True or False : The curve defined by , has a horizontal tangent line at .       True or False : The curve defined by , , has a vertical tangent line at .       Let Us Get Our Hands On     Sketch the graph of for and include the direction of the curve on your graph. Describe in words the path as increases from to .        Match the parametric equations with their corresponding graphs and give reasons for your choices.  The parametric equations match Graph # .  The parametric equations match Graph # .  The parametric equations match Graph # .  The parametric equations match Graph # .  The parametric equations match Graph # .  The parametric equations match Graph # .  The parametric equations match Graph # .  The parametric equations match Graph # .    Graphs to Match with Equations Above    Graph I  Graph II  Graph III    Graph IV  Graph V  Graph VI    Graph VII  Graph VIII            Find the length of the curve for .      Find the length of the curve for .        Find the length of one arch of the cycloid . A graph of the cycloid is provided below.  {Hint}: .           At what point(s) on the curve does the tangent line have slope ?      Set up, but do not evaluate, the integral that gives area of the surface obtained by rotating the curve for the axes indicated below.   about the -axis.    about the -axis.           Find the area enclosed by the cycloid when , and the -axis.  Hint:  and . Putting these together we get This will make integrating a bit easier.        Find the area enclosed by the loop of the curve .         Find the surface area of the object obtained by rotating for about the axis.       Now Grades Go Up     Let and . Then     .                         Set up, but do not evaluate, an integral that gives the surface area of the object obtained by rotating where about the axis. You may assume that the curve traces out exactly once for the given rangeof .                           The integral that correctly calculates the length of the curve defined by for is:                         Find the points on the curve where the tangenet line is horizontal.                            Helpful Formulas     If the curve is traced out once by the parametric equations and , then we can calculate the area under the curve with the formula .   If a curve is described by the parametric equations , , , where and are continuous on and is traversed exactly once as increases from to , then ≈   the length, , of is given by .    the surface area is give by . If the curve is rotated about the axis, then and . If the curve is rotated about the axis, then and .         "
+},
+{
+  "id": "session1wks-3-3-3",
+  "level": "2",
+  "url": "#session1wks-3-3-3",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "   True or False : The path represented by the parametric equations , where , is a circle of radius 1, with initial point at , terminal point at , and the path follows a counterclockwise direction.   "
+},
+{
+  "id": "session1wks-3-3-4",
+  "level": "2",
+  "url": "#session1wks-3-3-4",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "   True or False : The path represented by the parametric equations , where , is a circle of radius 1, with initial point at , terminal point at , and the path follows a clockwise direction.   "
+},
+{
+  "id": "session1wks-3-3-5",
+  "level": "2",
+  "url": "#session1wks-3-3-5",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "   True or False : The paths represented by and are the same paths.   "
+},
+{
+  "id": "session1wks-3-3-6",
+  "level": "2",
+  "url": "#session1wks-3-3-6",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "   True or False : The path represented by , where , starts at and moves in the clockwise direction twice around a circle centered at with a radius of 1.   "
+},
+{
+  "id": "session1wks-3-3-7",
+  "level": "2",
+  "url": "#session1wks-3-3-7",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "",
+  "body": "   True or False : Given and , the slope of the tangent for any value of is .   "
+},
+{
+  "id": "session1wks-3-3-8",
+  "level": "2",
+  "url": "#session1wks-3-3-8",
+  "type": "Worksheet Exercise",
+  "number": "6",
+  "title": "",
+  "body": "   True or False : Given and , the slope of the tangent line at is .   "
+},
+{
+  "id": "session1wks-3-3-9",
+  "level": "2",
+  "url": "#session1wks-3-3-9",
+  "type": "Worksheet Exercise",
+  "number": "7",
+  "title": "",
+  "body": "   True or False : The curve defined by , has a horizontal tangent line at .   "
+},
+{
+  "id": "session1wks-3-3-10",
+  "level": "2",
+  "url": "#session1wks-3-3-10",
+  "type": "Worksheet Exercise",
+  "number": "8",
+  "title": "",
+  "body": "   True or False : The curve defined by , , has a vertical tangent line at .   "
+},
+{
+  "id": "session1wks-3-4-2",
+  "level": "2",
+  "url": "#session1wks-3-4-2",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Sketch the graph of for and include the direction of the curve on your graph. Describe in words the path as increases from to .   "
+},
+{
+  "id": "session1wks-3-5-1",
+  "level": "2",
+  "url": "#session1wks-3-5-1",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Match the parametric equations with their corresponding graphs and give reasons for your choices.  The parametric equations match Graph # .  The parametric equations match Graph # .  The parametric equations match Graph # .  The parametric equations match Graph # .  The parametric equations match Graph # .  The parametric equations match Graph # .  The parametric equations match Graph # .  The parametric equations match Graph # .    Graphs to Match with Equations Above    Graph I  Graph II  Graph III    Graph IV  Graph V  Graph VI    Graph VII  Graph VIII       "
+},
+{
+  "id": "session1wks-3-6-1",
+  "level": "2",
+  "url": "#session1wks-3-6-1",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Find the length of the curve for .   "
+},
+{
+  "id": "session1wks-3-6-2",
+  "level": "2",
+  "url": "#session1wks-3-6-2",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  Find the length of the curve for .   "
+},
+{
+  "id": "session1wks-3-7-1",
+  "level": "2",
+  "url": "#session1wks-3-7-1",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "",
+  "body": "  Find the length of one arch of the cycloid . A graph of the cycloid is provided below.  {Hint}: .      "
+},
+{
+  "id": "session1wks-3-8-1",
+  "level": "2",
+  "url": "#session1wks-3-8-1",
+  "type": "Worksheet Exercise",
+  "number": "6",
+  "title": "",
+  "body": "  At what point(s) on the curve does the tangent line have slope ?   "
+},
+{
+  "id": "session1wks-3-8-2",
+  "level": "2",
+  "url": "#session1wks-3-8-2",
+  "type": "Worksheet Exercise",
+  "number": "7",
+  "title": "",
+  "body": "  Set up, but do not evaluate, the integral that gives area of the surface obtained by rotating the curve for the axes indicated below.   about the -axis.    about the -axis.      "
+},
+{
+  "id": "session1wks-3-9-1",
+  "level": "2",
+  "url": "#session1wks-3-9-1",
+  "type": "Worksheet Exercise",
+  "number": "8",
+  "title": "",
+  "body": "  Find the area enclosed by the cycloid when , and the -axis.  Hint:  and . Putting these together we get This will make integrating a bit easier.   "
+},
+{
+  "id": "session1wks-3-10-1",
+  "level": "2",
+  "url": "#session1wks-3-10-1",
+  "type": "Worksheet Exercise",
+  "number": "9",
+  "title": "",
+  "body": "  Find the area enclosed by the loop of the curve .      "
+},
+{
+  "id": "session1wks-3-10-2",
+  "level": "2",
+  "url": "#session1wks-3-10-2",
+  "type": "Worksheet Exercise",
+  "number": "10",
+  "title": "",
+  "body": "  Find the surface area of the object obtained by rotating for about the axis.   "
+},
+{
+  "id": "session1wks-3-11-2",
+  "level": "2",
+  "url": "#session1wks-3-11-2",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Let and . Then     .                      "
+},
+{
+  "id": "session1wks-3-11-3",
+  "level": "2",
+  "url": "#session1wks-3-11-3",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Set up, but do not evaluate, an integral that gives the surface area of the object obtained by rotating where about the axis. You may assume that the curve traces out exactly once for the given rangeof .                      "
+},
+{
+  "id": "session1wks-3-12-1",
+  "level": "2",
+  "url": "#session1wks-3-12-1",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  The integral that correctly calculates the length of the curve defined by for is:                      "
+},
+{
+  "id": "session1wks-3-12-2",
+  "level": "2",
+  "url": "#session1wks-3-12-2",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  Find the points on the curve where the tangenet line is horizontal.                        "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
